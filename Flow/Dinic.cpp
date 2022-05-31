0ef52c79c1b98ce8d0ac7259503fecc9d00b301c
BrehamPie <brehampie@gmail.com>
2022-05-31 12:32:21 +0600

    Added Initial Flow Templates

---
+++ Flow/Dinic.cpp
@@ -0,0 +1,72 @@
+/*.Complexity: O(V^2 E)
+.Call Dinic with total number of nodes.
+.Nodes start from 0.
+.Capacity is long long data.
+.make graph with create edge(u,v,capacity).
+.Get max flow with maxFlow(src,des).*/
+#define eb emplace_back
+struct Dinic {
+    struct Edge {
+        int u, v;
+        ll cap, flow = 0;
+        Edge() {}
+        Edge(int u, int v, ll cap) :u(u), v(v), cap(cap) {}
+    };
+    int N;
+    vector<Edge>edge;
+    vector<vector<int>>adj;
+    vector<int>d, pt;
+    Dinic(int N) :N(N), edge(0), adj(N), d(N), pt(N) {}
+    void addEdge(int u, int v, ll cap) {
+        if (u == v) return;
+        edge.eb(u, v, cap);
+        adj[u].eb(edge.size() - 1);
+        edge.eb(v, u, 0);
+        adj[v].eb(edge.size() - 1);
+    }
+    bool bfs(int s, int t) {
+        queue<int>q({ s });
+        fill(d.begin(), d.end(), N + 1);
+        d[s] = 0;
+        while (!q.empty()) {
+            int u = q.front();q.pop();
+            if (u == t) break;
+            for (int k : adj[u]) {
+                Edge& e = edge[k];
+                if (e.flow<e.cap && d[e.v]>d[e.u] + 1) {
+                    d[e.v] = d[e.u] + 1;
+                    q.emplace(e.v);
+                }
+            }
+        }
+        return d[t] != N + 1;
+    }
+    ll dfs(int u, int T, ll flow = -1) {
+        if (u == T || flow == 0) return flow;
+        for (int& i = pt[u];i < adj[u].size();i++) {
+            Edge& e = edge[adj[u][i]];
+            Edge& oe = edge[adj[u][i] ^ 1];
+            if (d[e.v] == d[e.u] + 1) {
+                ll amt = e.cap - e.flow;
+                if (flow != -1 && amt > flow) amt = flow;
+                if (ll pushed = dfs(e.v, T, amt)) {
+                    e.flow += pushed;
+                    oe.flow -= pushed;
+                    return pushed;
+                }
+            }
+        }
+        return 0;
+    }
+    ll maxFlow(int s, int t) {
+        ll total = 0;
+        while (bfs(s, t)) {
+            fill(pt.begin(), pt.end(), 0);
+            while (ll flow = dfs(s, t)) {
+                total += flow;
+            }
+        }
+        return total;
+    }
+
+};
---
+++ Flow/HopCroftKarp.cpp
@@ -0,0 +1,67 @@
+/*.Finds Maximum Matching In a bipartite graph
+.Complexity O(E√V)
+.1-indexed
+.No default constructor
+.add single edge for (u, v)*/
+struct HK {
+    static const int inf = 1e9;
+    int n;
+    vector<int>matchL, matchR, dist;
+    //matchL contains value of matched node for L part.
+    vector<vector<int>>adj;
+    HK(int n) :n(n), matchL(n + 1),
+        matchR(n + 1), dist(n + 1), adj(n + 1) {
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+    bool bfs() {
+        queue<int>q;
+        for (int u = 1;u <= n;u++) {
+            if (!matchL[u]) {
+                dist[u] = 0;
+                q.push(u);
+            }
+            else dist[u] = inf;
+        }
+        dist[0] = inf;///unmatched node matches with 0.
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (auto v : adj[u]) {
+                if (dist[matchR[v]] == inf) {
+                    dist[matchR[v]] = dist[u] + 1;
+                    q.push(matchR[v]);
+                }
+            }
+        }
+        return dist[0] != inf;
+    }
+
+    bool dfs(int u) {
+        if (!u) return true;
+        for (auto v : adj[u]) {
+            if (dist[matchR[v]] == dist[u] + 1
+                && dfs(matchR[v])) {
+                matchL[u] = v;
+                matchR[v] = u;
+                return true;
+            }
+        }
+        dist[u] = inf;
+        return false;
+    }
+    int max_match() {
+        int matching = 0;
+        while (bfs()) {
+            for (int u = 1;u <= n;u++) {
+                if (!matchL[u])
+                    if (dfs(u))
+                        matching++;
+            }
+        }
+        return matching;
+    }
+};
+

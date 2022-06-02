7808c2a3f77610ca0261921a9b3a1247b03408c9
emon <sm.emon002@gmail.com>
2022-06-02 16:14:08 +0600

    manacher geo

--- code/Misc/Template.cpp
+++ code/Misc/Template.cpp
@@ -5,10 +5,13 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
+using ll = long long;
+
 template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x, sep = ", "; return os << '}'; }
 void dbg_out() { cerr << endl; }
 template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << " " << H; dbg_out(T...); }
+
 #ifdef SMIE
 #define debug(args...) cerr << "(" << #args << "):", dbg_out(args)
 #else
@@ -20,11 +23,12 @@ ll powmod(ll a, ll b, ll MOD) { ll res = 1;a %= MOD;assert(b >= 0);for (; b; b >
 template <typename T>using orderedSet = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 //order_of_key(k) - number of element strictly less than k
 //find_by_order(k) - k'th element in set.(0 indexed)(iterator)
-using ll = long long;
+
 mt19937
 rng(chrono::steady_clock::now().time_since_epoch() .count());
 //uniform_int_distribution<int>(0, i)(rng)
-int main() {
+int main(int argc, char* argv[]) {
   ios_base::sync_with_stdio(false);//DON'T CC++
   cin.tie(NULL);//DON'T use for interactive
+  int seed = atoi(argv[1]);
 }

92d94f47e2c9a4e03315551f799af709498e252e
arnob918 <64248431+arnob918@users.noreply.github.com>
2022-06-02 10:49:55 +0600

    Update Points to be noted.cpp

--- code/Game Theory/Points to be noted.cpp
+++ code/Game Theory/Points to be noted.cpp
@@ -4,9 +4,9 @@
 >Bogus Nim = Nim
 >Staircase Nim = Odd indexed pile Nim (Even indexed pile doesnt matter, as one player can give bogus moves to drop all even piles to ground)
 >Sprague Grundy: [Every impartial game under the normal play convention is equivalent to a one-heap game of nim] 
->Every tree = one nim pile = tree root value; tree leaf value = 0; tree node value = mex of all child nodes.
+Every tree = one nim pile = tree root value; tree leaf value = 0; tree node value = mex of all child nodes.
 [Careful: one tree node can become multiple new tree roots(multiple elements in one node), then the value of that node = xor of all those root values]
-Hackenbush(Given a rooted tree; cut an edge in one move; subtree under that edge gets removed; last player to cut wins): 
+>Hackenbush(Given a rooted tree; cut an edge in one move; subtree under that edge gets removed; last player to cut wins): 
 Colon: //$G(u) = (G(v1)+1) \oplus (G(v2)+1) \oplus \cdots [v1, v2,\cdots$ are childs of u]
 For multiple trees ans is their xor
 >Hackenbush on graph (instead of tree given an rooted graph): 

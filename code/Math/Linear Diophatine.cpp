9a177099ccdda9afe5ce3775271881f14c0df033
Arnob Sarker <64248431+arnob918@users.noreply.github.com>
2022-10-11 09:04:26 +0600

    LDE te ektu vul chilo thik kore disi.
    16 line a if er second bracket chilona. r extended gcd add korchi.

--- code/Math/Linear Diophatine.cpp
+++ code/Math/Linear Diophatine.cpp
@@ -1,3 +1,10 @@
+int extended_gcd(ll a, ll b, ll& x, ll& y) {
+  if (b == 0){x = 1;y = 0;return a;}
+  ll x1, y1;
+  ll d = extended_gcd(b, a % b, x1, y1);
+  x = y1;y = x1 - y1 * (a / b);
+  return d;
+}
 /*x'=x+(k*B/g),y'=y-(k*A/g);infinite soln
 if A=B=0,C must equal 0 and any x,y is solution;
 if A|B=0,(x,y)=(C/A,k)|(k,C/B)*/
@@ -6,7 +13,7 @@ bool LDE(int A,int B,int C,int*x,int*y){
   if(C%g!=0)return false;
   int a=A/g,b=B/g,c=C/g;
   extended_gcd(a,b,x,y);//ax+by=1
-  if(g<0)a*=-1;b*=-1;c*=-1;//Ensure gcd(a,b)=1
+  if(g<0){a*=-1;b*=-1;c*=-1;}//Ensure gcd(a,b)=1
  *x*=c;*y*=c;//ax+by=c
   return true;//Solution Exists
 }

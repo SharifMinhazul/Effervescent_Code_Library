7afa40a3276205bc86487fc34e4fd9ef3aa1fc6d
Arnob Sarker <64248431+arnob918@users.noreply.github.com>
2022-10-11 15:39:30 +0600

    pointer to reference

--- code/Math/Linear Diophatine.cpp
+++ code/Math/Linear Diophatine.cpp
@@ -8,12 +8,12 @@ int extended_gcd(ll a, ll b, ll& x, ll& y) {
 /*x'=x+(k*B/g),y'=y-(k*A/g);infinite soln
 if A=B=0,C must equal 0 and any x,y is solution;
 if A|B=0,(x,y)=(C/A,k)|(k,C/B)*/
-bool LDE(int A,int B,int C,int*x,int*y){
+bool LDE(ll A,ll B,ll C,ll &x,ll &y){
   int g=gcd(A,B);
   if(C%g!=0)return false;
   int a=A/g,b=B/g,c=C/g;
   extended_gcd(a,b,x,y);//ax+by=1
   if(g<0){a*=-1;b*=-1;c*=-1;}//Ensure gcd(a,b)=1
- *x*=c;*y*=c;//ax+by=c
+  x*=c;y*=c;//ax+by=c
   return true;//Solution Exists
 }

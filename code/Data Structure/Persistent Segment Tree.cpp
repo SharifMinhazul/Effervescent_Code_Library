cacb470cdac0d8051a97ad92874fd08312024b4c
Arnob Sarker <64248431+arnob918@users.noreply.github.com>
2022-12-14 11:32:02 +0600

    Update Persistent Segment Tree.cpp

--- code/Data Structure/Persistent Segment Tree.cpp
+++ code/Data Structure/Persistent Segment Tree.cpp
@@ -1,53 +1,64 @@
-const int mxn = 1e5+5;//CHECK here for problem
+const int mxn = 4e5+5;
 int root[mxn], leftchild[25*mxn], rightchild[25*mxn], value[25*mxn], a[mxn];
-int now = 0, n;
+int now = 0, n, sz = 1;
+int l, r;
+
 int build(int L, int R){
 	int node = ++now;
-	if(L == R) return node;
+	if(L == R){
+		//initialize
+		//value[node] = a[L];
+		return node;
+	}
 	int mid = (L+R)>>1;
 	leftchild[node] = build(L, mid);
 	rightchild[node] = build(mid+1, R);
-	//initialize value[node]
+	//combine
+	//value[node] = value[leftchild[node]] + value[rightchild[node]];
 	return node;
 }
 
-int update(int nownode, int L, int R, int val){
+int update(int nownode, int L, int R, int ind, int val){
 	int node = ++now;
 	if(L == R){
-		//value[node] = value[nownode]+1;
+		//value[node] = value[nownode]+val;
 		//update value[node]
 		return node;
 	}
 	int mid = (L+R)>>1;
 	leftchild[node] = leftchild[nownode];
 	rightchild[node] = rightchild[nownode];
-	if(mid <= val){//change condition as required
-		leftchild[node] = update(leftchild[nownode], L, mid, val);
+	if(mid >= ind){//change condition as required
+		leftchild[node] = update(leftchild[nownode], L, mid, ind, val);
 	}
 	else{
-		rightchild[node] = update(rightchild[nownode], mid+1, R, val);
+		rightchild[node] = update(rightchild[nownode], mid+1, R, ind, val);
 	}
-	//value[node] = value[nownode]+1;
-	//update value[node]
+	//value[node] = value[leftchild[node]] + value[rightchild[node]];
+	//combine value[node]
 	return node;
 }
 
-int query(int leftnode, int rightnode, int L, int R, int k){
-	if(L==R) return L;
-	//int leftcnt = value[leftchild[rightnode]]-value[leftchild[leftnode]];a
-	//change as required
+
+int query(int nownode, int L, int R){
+	if(l > R || r < L) return 0;
+	
+	if(L>=l && r >= R){
+		return value[nownode];
+	}
 	int mid = (L+R)>>1;
-	if(leftcnt >= k){//change condition as required
-		return query(leftchild[leftnode], leftchild[rightnode], L, mid, k);
-	}
-	else{
-		return query(rightchild[leftnode], rightchild[rightnode], mid+1, R, k-leftcnt);
-	}
+	//change as required
+	return query(leftchild[nownode], L, mid) + query(rightchild[nownode], mid+1, R);
 }
 
-void persistentsegtree(){
-	root[0] = build(0, mxn);
-	for(int i=1; i<=n; i++){
-		root[i] = update(root[i-1], 0, mxn, a[i]);
+void persistant(){
+	root[0] = build(1, n);
+	while(m--){
+		if(ck == 2){
+			cout << query(root[idx], 1, n) << "\n";
+		}
+		else{
+		    	root[sz++] = update(root[idx], 1, n, ind, val);
+		}
 	}
 }

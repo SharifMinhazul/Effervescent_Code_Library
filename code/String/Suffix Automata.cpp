876fc6ec7baa0ae89d447ad097b2fff949f7ee7d
Sharif Minhazul Islam <sm.emon002@gmail.com>
2023-07-27 19:45:31 +0600

    change cht to dynamic cht and change suffix automata

--- code/String/Suffix Automata.cpp
+++ code/String/Suffix Automata.cpp
@@ -1,111 +1,103 @@
-const int MXCHR = 26;
+const int mxc = 26;
 /*
-take an object of suffixAutomata
-call extend(c) for each character c in string
-call Process() to initiate the important values
+  + link     - longest suffix belonging to another endpos-equivalent class.
+  + len      - largest string length ending in current state.
+  + firstPos - first occurance of substring ending at current state.
+  + adj      - suffix link tree.
+  + sz       - number of states.
+  + occ      - number of times state occured in string.
+  + dist     - number of distinct substring.
+  + cnt & SA - for count sorting the nodes.
 */
-struct suffixAutomata {
-/*
-len -> largest string length of the corresponding endpos-equivalent class
-link -> longest suffix that is another endpos-equivalent class
-firstpos -> end position of the first occurrence of the largest string of that node
-**/
-  struct state {
-    int link, len;
-    int next[MXCHR];
-    state() {}
-    state(int l) {
-      len = l;
-      link = -1;
-      for (int i = 0; i < MXCHR; i++) next[i] = -1;
+struct SuffixAutomata{
+  struct state{
+    int link, len, firstPos;
+    int next[mxc];
+    bool is_clone;
+    state(){}
+    state(int l){
+      len = l, link = -1;
+      is_clone = false;
+      for(int i=0;i<mxc;i++)next[i] = -1;
     }
   };
-  vector<state> node;
+  vector<state>t;
   int sz, last;
-  vector<int> cnt, distinct, firstPos, occur, SA;
-  vector<vector<int>> adj;  // suffix links tree
-                            // cnt and SA for counting sort the nodes.
-  int L;
-
-  suffixAutomata() {
-    node.push_back(state(0));
-    firstPos.push_back(-1);
-    occur.push_back(0);
-    last = 0;
-    sz = 0;
-    L = 0;
+  vector<ll>cnt,dist, occ,SA;
+  vector<vector<int>> adj;
+  SuffixAutomata(){
+    t.pb(state(0));
+    occ.pb(0);
+    last = sz = 0;
   }
-  int getID(char c) {
-    return c - 'a';  // change according to problem
-  }
-  void extend(char c) {
+  int getID(char c){ return c - 'a';}
+  void extend(char c){
     int idx = ++sz, p = last, id = getID(c);
-    L++;
-    node.push_back(state(node[last].len + 1));
-    firstPos.push_back(node[idx].len - 1);
-    occur.push_back(1);
-
-    while (p != -1 && node[p].next[id] == -1) {
-      node[p].next[id] = idx;
-      p = node[p].link;
+    t.pb(state(t[last].len + 1));
+    t[idx].firstPos = t[idx].len - 1;
+    occ.pb(1);
+    while(p!= -1 and t[p].next[id] == - 1){
+      t[p].next[id] = idx;
+      p = t[p].link;
     }
-    if (p == -1)
-      node[idx].link = 0;
-    else {
-      int q = node[p].next[id];
-      if (node[p].len + 1 == node[q].len)
-        node[idx].link = q;
-      else {
+    if(p==-1) t[idx].link = 0;
+    else{
+      int q = t[p].next[id];
+      if(t[p].len+1 == t[q].len) t[idx].link = q;
+      else{
         int clone = ++sz;
-        state x = node[q];
-        x.len = node[p].len + 1;
-        node.push_back(x);
-        firstPos.push_back(firstPos[q]);
-        occur.push_back(0);
-        while (p != -1 && node[p].next[id] == q) {
-          node[p].next[id] = clone;
-          p = node[p].link;
+        state x = t[q];
+        x.len = t[p].len+1;
+        t.pb(x);
+        t[clone].firstPos = t[q].firstPos;
+        t[clone].is_clone = true;
+        occ.pb(0);
+        while(p!=-1 and t[p].next[id]==q){
+          t[p].next[id] = clone;
+          p = t[p].link;
         }
-        node[idx].link = node[q].link = clone;
+        t[idx].link = t[q].link = clone;
       }
     }
     last = idx;
   }
-  void Process() {
-    cnt.resize(sz + 1);
-    distinct.resize(sz + 1);
-    SA.resize(sz + 1);
-    adj.resize(sz + 1);
-    for (int i = 0; i <= sz; i++) cnt[node[i].len]++;
-    for (int i = 1; i <= L; i++) cnt[i] += cnt[i - 1];
-    for (int i = 0; i <= sz; i++) SA[--cnt[node[i].len]] = i;
-    for (int i = sz; i > 0; i--) {
+  void build(string &s){
+    for(char c:s) extend(c);
+    cnt = dist = SA = vector<ll>(sz+1);
+    adj.resize(sz+1);
+    for(int i=0;i<=sz;i++)cnt[t[i].len]++;
+    for(int i=1;i<=sz;i++)cnt[i]+=cnt[i-1];
+    for(int i=0;i<=sz;i++) SA[--cnt[t[i].len]] = i;
+    
+    for(int i=sz;i>0;i--){
       int idx = SA[i];
-      occur[node[idx].link] += occur[idx];
-      adj[node[idx].link].push_back(idx);
-      distinct[idx] = 1;
-      for (int j = 0; j < MXCHR; j++) {
-        if (node[idx].next[j] != -1)
-          distinct[idx] += distinct[node[idx].next[j]];
+      occ[t[idx].link]+=occ[idx];
+      adj[t[idx].link].pb(idx);
+      dist[idx] = 1;
+      for(int j=0;j<mxc;j++){
+        if(t[idx].next[j]+1){
+          dist[idx]+=dist[t[idx].next[j]];
+        }
       }
-    }  // counts distinct substrings and occurance of each state
-    for (int i = 0; i < MXCHR; i++)
-      if (node[0].next[i] != -1) distinct[0] += distinct[node[0].next[i]];
+    }
+    for(int i=0;i<mxc;i++){
+      if(t[0].next[i]+1) dist[0]+=dist[t[0].next[i]];
+    }
   }
-  pair<int, int> lcs(string &str) {
-    int mxlen = 0, bestpos = -1, pos = 0, len = 0;
-    int u = 0;  // LCS of two string. returns start position and length
-    for (char c : str) {
+  pair<int,int> LCS( string& s){
+    int mxlen = 0, bestpos = -1, pos = 0,len = 0;
+    int u = 0;
+    for(char c:s){
       int v = getID(c);
-      while (u && node[u].next[v] == -1) {
-        u = node[u].link;
-        len = node[u].len;
+      while( u and t[u].next[v]!=-1){
+        u = t[u].link;
+        len = t[u].len;
       }
-      if (node[u].next[v] != -1) {
+      if(t[u].next[v]+1){
         len++;
-        u = node[u].next[v];
+        u = t[u].next[v];
       }
-      if (len > mxlen) {
+      if(len>mxlen){
         mxlen = len;
         bestpos = pos;
       }
@@ -113,6 +105,5 @@ firstpos -> end position of the first occurrence of the largest string of that n
     }
     return {bestpos - mxlen + 1, mxlen};
   }
-  state &operator[](int index) { return node[index]; }
+  state &operator[](int index) { return t[index];}
 };
-
